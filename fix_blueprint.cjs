3ef6fa4a108bbf1c18ecc5243a7e6ce5fda53ad0
Claudio Brignole <claudio@brignole.ch>
2026-05-10 11:24:59 +0200

    refactor: Remove i18n keys from code
    Replaced hardcoded i18n keys with static strings in various component files. This simplifies code and removes unnecessary dependencies on the translation system for static text.

--- fix_blueprint.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('firebase-blueprint.json', 'utf8');
-code = code.replace(/}\s*"Writer":/g, '},\n    "Writer":');
-fs.writeFileSync('firebase-blueprint.json', code);
--- fix_public_links.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/pages/PublicHome.tsx', 'utf8');
-code = code.replace(/href="#minimostre"/g, 'href="/minimostre"');
-code = code.replace(/href="#writers"/g, 'href="/writers"');
-code = code.replace(/href="#magazine"/g, 'href="/magazine"');
-fs.writeFileSync('src/pages/PublicHome.tsx', code);
